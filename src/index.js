e4e81d5b838c33638182cf973ec6183844531c80
Thiago Marinho <tgmarinho@gmail.com>
2019-09-05 10:47:49 -0400

    Setting Status Bar Light

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import './config/ReactotronConfig';
 import React from 'react';
+import { StatusBar } from 'react-native';
 import Routes from './routes';
 
 const App = () => {
-  return <Routes />;
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#090B22" />
+      <Routes />
+    </>
+  );
 };
 
 export default App;
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 
 // import { Container } from './styles';
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#090B22',
+  },
+});
+
 export default function Main() {
-  return <View />;
+  return <View style={styles.container} />;
 }
