8a2bad8644be06b05105d92331fd7743368fd41c
Thiago Marinho <tgmarinho@gmail.com>
2019-09-05 10:59:45 -0400

    Setting styled-components

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,15 +1,6 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
-
-// import { Container } from './styles';
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#090B22',
-  },
-});
+import { Container } from './styles';
 
 export default function Main() {
-  return <View style={styles.container} />;
+  return <Container />;
 }
